ab6807b6afbc10d65194dc93ac5e64859c9c1e61
syou <dbtnwl369@gmail.com>
2023-04-10 12:19:22 -0700

    change dependencies version

--- src/index.js
+++ src/index.js
@@ -21,11 +21,18 @@ import store from "./redux/store";
 // );
 const rootElement = document.getElementById('root');
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   rootElement
+// );
+
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>,
   rootElement
 );
