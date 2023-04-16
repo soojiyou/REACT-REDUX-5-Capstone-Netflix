e7f2268c599c9fa5363dfabee2fa574c01912f2b
syou <dbtnwl369@gmail.com>
2023-04-16 01:52:55 -0700

    mobile style changed

--- src/components/MovieSlide.js
+++ src/components/MovieSlide.js
@@ -7,19 +7,19 @@ import MovieCard from './MovieCard';
 const responsive = {
     superLargeDesktop: {
         // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
+        breakpoint: { max: 4000, min: 1200 },
         items: 5
     },
     desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 5
+        breakpoint: { max: 1200, min: 992 },
+        items: 4
     },
     tablet: {
-        breakpoint: { max: 1024, min: 464 },
+        breakpoint: { max: 992, min: 500 },
         items: 3
     },
     mobile: {
-        breakpoint: { max: 464, min: 0 },
+        breakpoint: { max: 500, min: 0 },
         items: 1
     }
 };
