99b85a4eebdbbeec6543d6d25f6e261011206f61
soojiyou <sooji.you@wsu.edu>
2023-04-19 13:07:13 -0700

    changed hover event for movies.js

--- src/components/MoviesCard.js
+++ src/components/MoviesCard.js
@@ -13,22 +13,25 @@ const MoviesCard = ({ item }) => {
     const { genreList } = useSelector(state => state.movie);
 
     return (
-        <div className='movielistcard' onClick={() => showMovieDetail(item.id)} style={{ backgroundImage: "url(" + `https://www.themoviedb.org/t/p/w300_and_h450_bestv2${item.poster_path}` + ")" }}>
+        <div className='moviecard-backgroud'>
+            <div className='movielistcard' onClick={() => showMovieDetail(item.id)} style={{ backgroundImage: "url(" + `https://www.themoviedb.org/t/p/w300_and_h450_bestv2${item.poster_path}` + ")" }}>
 
-            <div className='overlay-moviecard '>
-                <h1>{item.title}</h1>
-                <div>
-                    {item.genre_ids.map(id =>
-                        <Badge key={id} bg="danger">{genreList.find(item => item.id === id).name}</Badge>
-                    )}
-                </div>
+                <div className='overlay-moviecard '>
+                    <h1>{item.title}</h1>
+                    <div>
+                        {item.genre_ids.map(id =>
+                            <Badge key={id} bg="danger">{genreList.find(item => item.id === id).name}</Badge>
+                        )}
+                    </div>
 
-                <div>
-                    <span><FontAwesomeIcon icon={faFilm} /> {item.vote_average} </span>
-                    <span> <FontAwesomeIcon icon={faUsers} /> {item.vote_count} </span>
-                    <span className='adultfont'> {item.adult ? "Adult" : "Under 18"} </span>
-                </div>
-            </div></div>
+                    <div>
+                        <span><FontAwesomeIcon icon={faFilm} /> {item.vote_average} </span>
+                        <span> <FontAwesomeIcon icon={faUsers} /> {item.vote_count} </span>
+                        <span className='adultfont'> {item.adult ? "Adult" : "Under 18"} </span>
+                    </div>
+                </div></div>
+        </div>
+
     )
 }
 
